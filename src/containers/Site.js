fd470cd9eb65fd665fade3e4eba6e3c816378d6d
c0b4lt-student <Corentin.turgis@outlook.fr>
2023-04-28 10:22:52 +0200

    register page added for test

--- src/containers/Site.js
+++ src/containers/Site.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import LogIn from "../pages/LogIn";
+import Register from "../pages/Register";
 
 function Site(props) {
   return (
@@ -8,6 +9,7 @@ function Site(props) {
       <Routes>
         <Route exact path="/" element={<LogIn />} />
         <Route exact path="/login" element={<LogIn />} />
+        <Route exact path="/register" element={<Register />} />
 
         <Route path='*' element={<div>La page demandée n'existe pas</div>} />
       </Routes>
---
+++ src/pages/Register.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import LoginButton from "../components/LoginButton";
+import axios from 'axios';
+
+function Register(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const postReq = async () => {
+    return (await axios({
+      method: 'post',
+      url: 'http://localhost:8080/api/auth/register',
+      data : {
+        email: email,
+        password: password
+      }
+    }));
+  };
+
+  return (
+    <>
+      <h1>Page de connexion</h1>
+      <form>
+        <label htmlFor="username">Nom d'utilisateur :</label>
+        <input type="email" id="username" name="username"
+               onChange={(e) => setEmail(e.currentTarget.value)}/>
+
+        <label htmlFor="password">Mot de passe :</label>
+        <input type="password" id="password" name="password"
+               onChange={(e) => setPassword(e.currentTarget.value)}/>
+
+        <LoginButton type="button">{postReq}</LoginButton>
+      </form>
+    </>
+  );
+}
+
+export default Register;
